0c48210cb6a71238e9339fb5b1f3f0a83b7ed395
robyzhou <piratest2006@gmail.com>
2017-09-22 17:48:35 +0800

    Add iPhone8 / iPhone X support
    Add iPhone8 / iPhone X support

--- SDVersion/SDiOSVersion/SDiOSVersion.h
+++ SDVersion/SDiOSVersion/SDiOSVersion.h
@@ -22,30 +22,33 @@ typedef NS_ENUM(NSInteger, DeviceVersion){
     iPhone6SPlus          = 11,
     iPhone7               = 12,
     iPhone7Plus           = 13,
-    iPhoneSE              = 14,
+    iPhone8               = 14,
+    iPhone8Plus           = 15,
+    iPhoneX               = 16,
+    iPhoneSE              = 17,
     
-    iPad1                 = 15,
-    iPad2                 = 16,
-    iPadMini              = 17,
-    iPad3                 = 18,
-    iPad4                 = 19,
-    iPadAir               = 20,
-    iPadMini2             = 21,
-    iPadAir2              = 22,
-    iPadMini3             = 23,
-    iPadMini4             = 24,
-    iPadPro12Dot9Inch     = 25,
-    iPadPro9Dot7Inch      = 26,
-    iPad5                 = 27,
-    iPadPro12Dot9Inch2Gen = 28,
-    iPadPro10Dot5Inch     = 29,
+    iPad1                 = 18,
+    iPad2                 = 19,
+    iPadMini              = 20,
+    iPad3                 = 21,
+    iPad4                 = 22,
+    iPadAir               = 23,
+    iPadMini2             = 24,
+    iPadAir2              = 25,
+    iPadMini3             = 26,
+    iPadMini4             = 27,
+    iPadPro12Dot9Inch     = 28,
+    iPadPro9Dot7Inch      = 29,
+    iPad5                 = 30,
+    iPadPro12Dot9Inch2Gen = 31,
+    iPadPro10Dot5Inch     = 32,
     
-    iPodTouch1Gen         = 30,
-    iPodTouch2Gen         = 31,
-    iPodTouch3Gen         = 32,
-    iPodTouch4Gen         = 33,
-    iPodTouch5Gen         = 34,
-    iPodTouch6Gen         = 35
+    iPodTouch1Gen         = 33,
+    iPodTouch2Gen         = 34,
+    iPodTouch3Gen         = 35,
+    iPodTouch4Gen         = 36,
+    iPodTouch5Gen         = 37,
+    iPodTouch6Gen         = 38
 };
 
 typedef NS_ENUM(NSInteger, DeviceSize){
@@ -53,7 +56,8 @@ typedef NS_ENUM(NSInteger, DeviceSize){
     Screen3Dot5inch = 1,
     Screen4inch     = 2,
     Screen4Dot7inch = 3,
-    Screen5Dot5inch = 4
+    Screen5Dot5inch = 4,
+    Screen5Dot8inch = 5
 };
 
 @interface SDiOSVersion : NSObject
